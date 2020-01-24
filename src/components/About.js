7d4be0aaed79cb994f0374ad94a3131258054681
ashwin <ashwinnarayanan09@gmail.com>
2020-01-24 14:12:27 +0100

    payment options

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,7 @@ function About() {
 
 
     return (
-        <div className="container"><br/><br/>
-
+        <div className="container">
         <div className ="jumbotron">
             We are a Meerut, India based organisation and our mission is to create a better world for animals.<br/>
             "RESCUE - There's a "U" in the word for a reason"
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer(){
 
     return(
 
-        <Nav activeKey="1" className="navbar navbar-expand-lg navbar-light bg-dark ">
+        <Nav activeKey="1" className="navbar navbar-expand-lg navbar-light bg-dark fixed-bottom">
             <p style={{color:"white"}}>Follow us on social media</p>&nbsp;
             <SocialIcon url="https://www.facebook.com/groups/534963220195670/?ref=share" />&nbsp;
             <SocialIcon url="https://www.instagram.com/_rehem_/?igshid=vxf0m3d9mtns" />&nbsp;
