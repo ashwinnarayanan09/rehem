b9acd1efe7e6ec5f805ec8755df6ec2b92a96b82
Ashwin Narayanan <Ashwin.Narayanan@tre.se>
2020-12-01 12:40:59 +0100

    background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 function Home() {
-    return (
-        <div className="container p-3 my-3 text-white" style={{overflowY: "auto"}}>
-            <h2>Rehem Foundation</h2>
-            <h5>Helping Speechless</h5><br/><br/>
-                <img src ="images/rehem.jpeg" width="300px"/><br/><br/>
-        </div>
-
-    );
+  return (
+    <div
+      className="container bg-primary p-3 my-3 text-white"
+      style={{ overflowY: "auto" }}
+    >
+      <h2>Rehem Foundation</h2>
+      <h5>Helping Speechless</h5>
+      <br />
+      <br />
+      <img src="images/rehem.jpeg" width="300px" />
+      <br />
+      <br />
+    </div>
+  );
 }
 
 export default Home;
