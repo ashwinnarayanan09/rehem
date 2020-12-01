2feece592b28304606f83d0c27bcb5fb083c9800
Ashwin Narayanan <Ashwin.Narayanan@tre.se>
2020-12-01 09:53:52 +0100

    test netlify

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,38 +1,47 @@
 import React from "react";
 import CustomizedTables from "./CustomisedTables";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function Donate() {
-    return (
+  return (
+    <div className="container">
+      <Tabs>
+        <TabList>
+          <Tab>
+            <img src="images/paytm.png"></img>
+          </Tab>
+          <Tab>
+            <img src="images/gpay.png"></img>
+          </Tab>
+          <Tab>
+            <img src="images/phonepe.png"></img>{" "}
+          </Tab>
+          <Tab>
+            <img src="images/paypal.png"></img>
+          </Tab>
+        </TabList>
 
-
-        <div className="container">
-            <Tabs>
-                <TabList>
-                    <Tab><img src ="images/paytm.png"></img></Tab>
-                    <Tab><img src ="images/gpay.png"></img></Tab>
-                    <Tab><img src ="images/phonepe.png"></img>  </Tab>
-                    <Tab><img src ="images/paypal.png"></img></Tab>
-                </TabList>
-
-                <TabPanel>
-                    <img src = "images/paytm.jpeg" height="500px"/>
-                </TabPanel>
-                <TabPanel>
-                    <h2>+91 6395125464</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>+91 6395125464</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2><a href="https://www.paypal.me/rehem"> https://www.paypal.me/rehem</a> </h2>
-                </TabPanel>
-            </Tabs>
-        </div>
-
-
-    );
+        <TabPanel>
+          <img src="images/paytm.jpeg" height="500px" />
+        </TabPanel>
+        <TabPanel>
+          <h2>+91 6395125464</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>+91 6395125464</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>
+            <a href="https://www.paypal.me/rehem">
+              {" "}
+              https://www.paypal.me/rehem
+            </a>{" "}
+          </h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 }
 
-export default Donate ;
+export default Donate;
